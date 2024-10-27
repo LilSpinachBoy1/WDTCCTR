a1d1629bb0469e271ac7a55978c331aeee095700
LilSPinachBoy1 <sam@moocows.co.uk>
2024-10-27 22:37:56 +0000

    Created conversions class

---
+++ Modules/conversions.py
@@ -0,0 +1,32 @@
+"""
+CONVERSIONS MODULE: Holds a class to handle conversions between percentages and coordinate values
+"""
+
+
+class Conversions:
+    # Get the screen data to keep locally
+    def __init__(self, screen_data):
+        self.screen_width = screen_data[0][0]
+        self.screen_height = screen_data[0][1]
+
+    # Take a pair of coordinates and convert them TO PIXEL VALUES
+    def dual_pe_to_pi(self, coords: (float, float)) -> (int, int):
+        pi_width = (coords[0] / 100) * self.screen_width
+        pi_height = (coords[1] / 100) * self.screen_height
+        return int(pi_width), int(pi_height)
+
+    # Take a pair of coordinates and convert them TO PERCENTAGE VALUES
+    def dual_pi_to_pe(self, coords: (int, int)) -> (float, float):
+        pe_width = (coords[0] / self.screen_width) * 100
+        pe_height = (coords[1] / self.screen_height) * 100
+        return pe_width, pe_height
+
+    # Take a single percentage, and convert it TO A PIXEL VALUE
+    def pe_to_pi(self, pe: float, is_width: bool) -> int:
+        mult = self.screen_width if is_width else self.screen_height
+        return int((pe / 100) * mult)
+
+    # Take a single pixel pos, and convert it TO A PERCENTAGE
+    def pi_to_pe(self, pi: int, is_width: bool) -> float:
+        div = self.screen_width if is_width else self.screen_height
+        return (pi / div) * 100
