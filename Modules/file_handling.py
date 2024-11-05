3924f7792d92690a9fbaf799a52d6afc56fc7e39
LilSPinachBoy1 <sam@moocows.co.uk>
2024-11-05 12:21:53 +0000

    Implimented file writing and reading using json and dicts

--- Modules/file_handling.py
+++ Modules/file_handling.py
@@ -1,6 +1,7 @@
 """
 FILE HANDLING MODULE: Holds functions to read and write to files
 """
+import json
 
 # File location constants. NOTE: Files relative to main.py, not here
 USERDATA_ROOT = "UserData/"
@@ -15,35 +16,18 @@ lines = 1
 
 
 # Read settings
-#TODO: Change to just read the dict from the file, and then return to non testing form
 def read_settings() -> dict:
     # Get all the data from settings
-    settings_addr = TESTING_ROOT + SETTINGS
+    settings_addr = USERDATA_ROOT + SETTINGS
     file = open(settings_addr, "r")
-
-    # Get screen resolution values and use location of comma to convert to usable values
-    screen_res = file.readline()
-    comma_index = 0
-    for i in range(len(screen_res)):
-        if screen_res[i] == ",":
-            comma_index = i
-    screen_x, screen_y = screen_res[:comma_index], screen_res[comma_index+1:]
-
-    # Close file, as we are finished
+    data = json.loads(file.read())
     file.close()
-
-    # Store the data in a dict
-    return {
-        "screen_res": (int(screen_x), int(screen_y))
-    }
+    return data
 
 
 def write_settings(data: dict) -> None:
     settings_addr = USERDATA_ROOT + SETTINGS
     file = open(settings_addr, "w")
-    file.write(str(data))
+    data = json.dumps(data)
+    file.write(data)
     file.close()
-
-
-t_data = read_settings()
-print(t_data["screen_res"])
