f5cdca2ed3065b0e6bf3120600a68a22f9b9d604
LilSPinachBoy1 <sam@moocows.co.uk>
2024-11-15 21:27:03 +0000

    Snail can now ✨spin✨

--- Modules/characters.py
+++ Modules/characters.py
@@ -30,6 +30,8 @@ class Player(pygame.sprite.Sprite):
         # Movement based attributes
         self.is_moving = False  # Used to control animations
         self.vertical_movement = 0
+        self.speed = 1
+        self.direction = "+"
 
     def process_movement(self):
         # Set flag for if movement is occurring, in order to determine if an animation should run
@@ -37,14 +39,21 @@ class Player(pygame.sprite.Sprite):
         self.is_moving = any(pressed[key] for key in movement_keys)  # Sets true if any of the keys in movement_keys are currently pressed
 
         # Move the sprite based on inputs
+        new_direction = self.direction
         if pressed[K_d]:
-            self.vertical_movement = 1
+            self.vertical_movement = self.speed
+            new_direction = "+"
         elif pressed[K_a]:
-            self.vertical_movement = -1
+            self.vertical_movement = -self.speed
+            new_direction = "-"
         self.coords[0] += self.vertical_movement  # Change x coordinate based on vertical movement variable
         self.rect.bottomleft = self.coords  # Apply coordinate change to rect position
-
         self.vertical_movement = 0  # End movement if no keys are pressed
+
+        # Determine if image needs to be flipped
+        if self.direction != new_direction:
+            self.direction = new_direction
+            self.image = pygame.transform.flip(self.image, flip_y=False, flip_x=True)
 
     def out(self):
         self.process_movement()
